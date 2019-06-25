86a2b532090c3f45876a5ab978f53060e0ab02fe
Colin Ritchey <colinritchey256@gmail.com>
2019-06-24 19:51:43 -0700

    added the closeModal function and the ability to close the modal on the outside of the modal

--- js/index.js
+++ js/index.js
@@ -55,10 +55,38 @@ function openModal (el) {
 
   img.setAttribute('src', link);
   // img.setAttribute('class', 'thumbnail-card');
-  img.setAttribute('width', '100px');
-  img.setAttribute('height', '100px');
   
   modal.appendChild(img);
 
-  // console.log(el.target.getAttribute('src'));
+  document.body.style.overflow = 'hidden';
+  
+  const closeModalButton = document.getElementById('close-modal-button');
+  closeModalButton.addEventListener('click', closeModal, true);
+  
+  /** Close Modal if outside click */
+  modalContainer.addEventListener('click', handleOutsideClick, true);
+}
+
+function handleOutsideClick (el) {
+  if (document.getElementById('modal').contains(event.target)) {
+    console.log('clicked inside');
+  } else {
+    console.log('clicked outside');
+    closeModal();
+  }
+}
+
+function closeModal (el) {
+  // el.preventDefault();
+  document.body.style.overflow = 'auto';
+  
+  const modal = document.getElementById('modal');
+  const img = modal.getElementsByTagName('img')[0];
+
+  modal.removeChild(img);
+
+  const modalContainer = document.getElementById('modal-container');
+  modalContainer.style.visibility = 'hidden';
+
+  const closeModalButton = document.getElementById('close-modal-button');
 }
